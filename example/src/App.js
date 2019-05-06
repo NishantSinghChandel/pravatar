58888c877e0a4d9ecb217068297313010d397324
realnishantsingh <nishant@grappus.com>
2019-05-06 20:02:08 +0530

    [Initilized]

--- example/src/App.js
+++ example/src/App.js
@@ -1,13 +1,19 @@
-import React, { Component } from 'react'
-
-import ExampleComponent from 'pravatar'
+import React, { Component } from "react";
+import Pravatar from "pravatar";
 
 export default class App extends Component {
-  render () {
+  render() {
     return (
       <div>
-        <ExampleComponent text='Modern React component module' />
+        <Pravatar
+          text=""
+          bg=""
+          width="100px"
+          height="100px"
+          radius="100px"
+          fontSize="50px"
+        />
       </div>
-    )
+    );
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,22 +1,37 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-
 import styles from './styles.css'
 
 export default class ExampleComponent extends Component {
   static propTypes = {
-    text: PropTypes.string
+    text: PropTypes.string,
+    bg: PropTypes.string,
+    height: PropTypes.string,
+    width: PropTypes.string,
+    radius: PropTypes.string,
+    fontSize: PropTypes.string
   }
 
   render() {
-    const {
-      text
-    } = this.props
+    const { text } = this.props
+    const { bg } = this.props
+    const { width } = this.props
+    const { height } = this.props
+    const { radius } = this.props
+    const { fontSize } = this.props
 
     return (
-      <div className={styles.test}>
-        Example Component: {text}
-      </div>
+      <div
+        className={styles.pravatar}
+        text={text}
+        style={{
+          backgroundImage: `url(${bg})`,
+          width: `${width}`,
+          height: `${height}`,
+          borderRadius: `${radius}`,
+          fontSize: `${fontSize}`
+        }}
+      />
     )
   }
 }
